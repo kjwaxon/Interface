ea37a80f7b0de9645a1af4dacb7c2d39824a3f7e
kjwaxon <kaanaslan1996@hotmail.com>
2022-04-19 14:25:25 +0300

    Interface

---
+++ Ödev1804/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Ödev1804
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            X nesne = new X();
+            nesne.toplama(5,6);
+            nesne.çarpma(8,16);
+            
+        }
+    }
+}
---
+++ Ödev1804/İşlem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ödev1804
+{
+    public interface İşlem
+    {
+        void toplama(int a,int b);
+        void çarpma(int a,int b);
+
+    }
+      
+    class X : İşlem
+    {
+        int n, m;
+       public void toplama(int a, int b)
+        {
+            n = a + b;
+            Console.WriteLine("2 sayının toplamı : "+n);
+        }
+                public void çarpma(int a, int b)
+        {
+            m = a * b;
+            Console.WriteLine("2 sayının çarpımı : "+ m);
+        }
+
+    }
+}
